933f93f1524049cbba6f640e5494670aa1f220c1
Anton Asabin <anton.asabin@gmail.com>
2023-01-26 21:49:08 +0500

    Insert settings.py

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ load_dotenv(find_dotenv())
 
 
 def add_token_bot(name_bot: str, token: str):
+    # TODO проверить
     if os.environ.get(name_bot) is None:
         set_key(find_dotenv(), name_bot, token)
         return True
@@ -13,6 +14,7 @@ def add_token_bot(name_bot: str, token: str):
 
 
 if __name__ == '__main__':
+    # TODO Тестовый, скорее всего изменить надо
     while True:
         namebot = input('Введите имя бота: ')
         if os.environ.get(namebot) is None:
